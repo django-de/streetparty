c3fdabec289c2aa3e918f7437e8b9c3a27e21c5b
Florian Apolloner <florian@apolloner.eu>
2013-09-16 18:53:56 +0200

    .

--- streetparty/management/commands/update_streetparty_data.py
+++ streetparty/management/commands/update_streetparty_data.py
@@ -1,10 +1,10 @@
-from django.core.management.base import NoArgsCommand
+from django.core.management.base import BaseCommand
 
 from streetparty.helper import StrassenFestHelper
 
 
-class Command(NoArgsCommand):
+class Command(BaseCommand):
     help = 'Holt aktuelle Daten von den Verwaltungsseiten'
 
-    def handle_noargs(self, **options):
+    def handle(self, *args, **options):
         StrassenFestHelper().update()
