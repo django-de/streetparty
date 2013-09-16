58b61c5bcc8a6ffd0c1491298196448eca3644dc
Florian Apolloner <florian@apolloner.eu>
2013-09-16 19:13:12 +0200

    .

--- streetparty/views.py
+++ streetparty/views.py
@@ -7,13 +7,16 @@ from .models import StrassenFest
 class StrassenFestList(ListView):
     def get_queryset(self):
         qs = StrassenFest.objects.order_by('von')
-        form_data = self.request.GET
+        form_data = self.request.POST
         if 'von' in form_data and 'bis' in form_data:
             qs = qs.filter(von__gte=form_data['von'], bis__lte=form_data['bis'])
         else:
             today = timezone.now()
             qs = qs.filter(von__year=today.year, von__month=today.month)
         return qs
+
+    def post(self, *args, **kwargs):
+        return self.get(*args, **kwargs)
 
 class StrassenFestListListing4(ListView):
     def get_queryset(self):
