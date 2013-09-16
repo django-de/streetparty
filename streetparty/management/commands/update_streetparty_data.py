c2a6113d2be713b11a54b148a56df7dc3f9f1e21
Florian Apolloner <florian@apolloner.eu>
2013-09-16 18:14:57 +0200

    Minimal validation of input data.

--- streetparty/management/commands/update_streetparty_data.py
+++ streetparty/management/commands/update_streetparty_data.py
@@ -1,9 +1,11 @@
+import sys
 import json
 import urllib2
 from datetime import datetime
 
 from django.conf import settings
 from django.core.management.base import NoArgsCommand
+from django.core.exceptions import ValidationError
 
 from streetparty.models import StrassenFest
 
@@ -17,7 +19,16 @@ class Command(NoArgsCommand):
         source = opener.open(req)
 
         for data in json.load(source)['index']:
-            data['bis'] = datetime.strptime(data["bis"], "%d.%m.%Y")
+            # This API is so broken.
+            data['von'] = datetime.strptime(data['von'], '%Y-%m-%d')
+            data['bis'] = datetime.strptime(data['bis'], '%d.%m.%Y')
             # save figures out whether to update or to insert by using
             # the remote id as primary key.
-            StrassenFest(**data).save()
+            fest = StrassenFest(**data)
+            try:
+                fest.full_clean(exclude=['id'])
+            except ValidationError as e:
+                print >>sys.stderr, 'Failed to validate remote entry with id %s' % data['id']
+                print >>sys.stderr, str(e)
+            else:
+                fest.save()
